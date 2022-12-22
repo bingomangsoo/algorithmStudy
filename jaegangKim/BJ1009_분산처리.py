45a3cc1c131b4b4611d449856cacc74ac882d44b
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2022-12-22 12:48:51 +0900

    Update BJ1009_분산처리.py

--- jaegangKim/BJ1009_분산처리.py
+++ jaegangKim/BJ1009_분산처리.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
+# 첫번째 방법
 
 numList = [[10,10,10,10],[1,1,1,1],[6,2,4,8],[1,3,9,7],[6,4,6,4],
            [5,5,5,5],[6,6,6,6],[1,7,9,3],[6,8,4,2],[1,9,1,9]]
@@ -13,8 +9,9 @@ for i in range(t):
     print(numList[a][b%4])
 
 
-# In[ ]:
 
+
+# 더 짧게 작성한 코드 ( 시간도 단축 됨 )
 
 t = int(input())
 for i in range(t):
@@ -22,10 +19,3 @@ for i in range(t):
     a = (a%10 if a%10 else 10)
     b = (b%4 if b%4 else 4)
     print(a**b%10 if a**b%10 else 10)
-
-
-# In[ ]:
-
-
-
-
