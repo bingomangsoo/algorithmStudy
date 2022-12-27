ea04c67b31b369eaed502069c2ae953ca9c5c56f
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2022-12-27 19:43:06 +0900

    Add files via upload

---
+++ jaegangKim/BJ8958_OX퀴즈.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+t = int(input())
+for i in range(t):
+    ox = input()
+    score = 0
+    o = 1
+    for j in ox:
+        if j =='O':
+            score += o
+            o += 1
+        else:
+            o=1
+    print(score)
+
+
+# In[ ]:
+
+
+
+
