05585968f45af727251216aa27112b732e9f12ce
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2022-12-27 20:24:39 +0900

    Add files via upload

---
+++ jaegangKim/BJ4344_평균은 넘겠지.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[37]:
+
+
+c = int(input())
+scoreList = []
+for i in range(c):
+    scoreList.append(list(map(int,input().split())))
+for score in scoreList:
+    mean = sum(score[1:])/score[0]
+    count = 0 
+    for num in score[1:]:
+        if num>mean:
+            count+=1
+    p = count/score[0]*100
+    print('%.3f'%p,end='')
+    print('%')
+
+
+# In[ ]:
+
+
+
+
