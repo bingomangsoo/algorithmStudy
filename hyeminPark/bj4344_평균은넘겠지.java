930a05df31300a3b6d2882855621f6923b4ebaac
hyemin <101463273+bingomangsoo@users.noreply.github.com>
2022-12-27 20:41:05 +0900

    Add files via upload

---
+++ hyeminPark/bj4344_평균은넘겠지.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class bj4344_평균은넘겠지 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+
+        for (int i=0; i<n1; i++){
+            int n2 = sc.nextInt();
+            int [] arr = new int[n2];
+
+            int sum = 0;
+            for (int j=0; j<n2; j++){
+                int n3 = sc.nextInt();
+                arr[j] = n3;
+                sum+= n3;
+            }
+
+            int count = 0;
+            for (int x : arr){
+                if (x > sum/n2) {
+                    count++;
+                }
+            }
+
+            System.out.println(String.format("%.3f", count/(double)n2*100*1000/1000.0));
+            System.out.print("%");
+
+        }
+    }
+}
