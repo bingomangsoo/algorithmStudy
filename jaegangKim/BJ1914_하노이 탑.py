baade2b2e906dfe4993cde5894bc4745dc023aca
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2023-01-02 20:19:59 +0900

    Update BJ1914_하노이 탑.py

--- jaegangKim/BJ1914_하노이 탑.py
+++ jaegangKim/BJ1914_하노이 탑.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[34]:
-
 
 n = int(input())
+
+# 이동 횟수 구하기
 def tower(i):
     if i==1:
         return 1
     else:
-        return 2**n -1#1->2 장대로 이동 + 제일 긴 막대가 1->3 + 2->3 장대로 이동
+        return (2**n) -1    # tower(n-1)+tower(n-1)+1  
+
+# 이동 방법 구하기
 def order(n):
     if n ==1:
         return "1 3\n"
@@ -17,7 +16,8 @@ def order(n):
         tmp1 = order(n-1).replace("2","t").replace("3","2").replace("t","3")
         tmp2 = order(n-1).replace("2","t").replace("1","2").replace("t","1")
         return tmp1+order(1)+tmp2
+    
 print(tower(n))
+
 if n<=20:
     print(order(n))
-
