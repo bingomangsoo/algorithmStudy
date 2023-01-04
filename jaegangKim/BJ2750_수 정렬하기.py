c6a1759ebbde1bff13353b8add3f2d4de0c823f4
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2023-01-04 17:37:07 +0900

    Add files via upload

---
+++ jaegangKim/BJ2750_수 정렬하기.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+n = int(input())
+numList = []
+for i in range(n):
+    numList.append(int(input()))
+numList.sort()
+for j in numList:
+    print(j)
+
+
+# In[ ]:
+
+
+
+
