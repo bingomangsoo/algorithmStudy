28de40f1811cf03b3d823c234f77818f0cdf77d6
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2023-01-17 19:25:59 +0900

    Update BJ10819_차이를 최대로.py

--- jaegangKim/BJ10819_차이를 최대로.py
+++ jaegangKim/BJ10819_차이를 최대로.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 from itertools import permutations as pm
 n = int(input())
@@ -19,10 +14,4 @@ for p in pm(a):
     maxNum = max(maxNum,cal(p))
     
 print(maxNum)
-
-
-# In[ ]:
-
-
-
 
