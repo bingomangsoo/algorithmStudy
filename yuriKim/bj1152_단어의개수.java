524941ec6b06e92b2e0f7f7d604b1b0d30a94841
PIZZAY <108074336+kimpizza@users.noreply.github.com>
2023-01-17 16:17:20 +0900

    Add files via upload

---
+++ yuriKim/bj1152_단어의개수.java
@@ -0,0 +1,25 @@
+package java_study;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bj1152_단어의개수 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String [] arr = str.split(" ");
+
+        int count = 0;
+        for (String a : arr){
+            if (a.equals("")){
+                count++;
+            }
+        }
+        System.out.println(arr.length-count);
+    }
+}
+
+
