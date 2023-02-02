bc05ea60911be0414db9ddb14a0c480bc408a4f6
PIZZAY <108074336+kimpizza@users.noreply.github.com>
2023-02-02 20:30:05 +0900

    Add files via upload

--- yuriKim/class2/bj2775_부녀회장이될테야.java
+++ yuriKim/class2/bj2775_부녀회장이될테야.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 
 public class bj2775_부녀회장이될테야 {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
+		
+		// 아파트 생성 
+		int[][] APT = new int[15][15];
+ 
+		for(int i = 0; i < 15; i++) {
+			APT[i][1] = 1;	// i층 1호
+			APT[0][i] = i;	// 0층 i호
+		}
+ 
+ 
+		for(int i = 1; i < 15; i ++) {	// 1층부터 14층까지
+ 
+			for(int j = 2; j < 15; j++) {	// 2호부터 14호까지
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+		
+		// 테스트 부분 		
 		int T = sc.nextInt();
-		for(int i = 0; i<T; i++) {
+		
+		for(int i = 0; i < T; i++) {
 			int k = sc.nextInt();
 			int n = sc.nextInt();
-			int [][] arr = new int [k+1][n];
-			int depth = 1;
-			
-			for(int p = 0;p <=k;p++) { //층 
-				for(int q = 1;q<n;q++) { //호 
-					for(int z = 1;z<n; z++) {
-						if(z == n){
-							arr[p][q] = z+depth;
-							depth++;
-							break;
-						}
-						if(z == 1) {
-							arr[p][q] = 1;
-						}else {
-							arr[p][q] = z+depth;
-						}
-					}
-				}
-			}	
+			System.out.println(APT[k][n]);
 		}
 	}
+ 
 }
+ 
