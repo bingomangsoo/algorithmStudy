c3bfbd21631bb5da1a536d5b0ccbaae2d0aa0f24
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-02-15 21:19:53 +0900

    Add files via upload

---
+++ hyeminPark/class2/bj11650_좌표정렬하기.java
@@ -0,0 +1,40 @@
+package commit;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class bj11650_좌표정렬하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int xy [][] = new int[n][2];
+        for (int i =0; i<n; i++){
+            String str = br.readLine();
+            xy[i][0] = Integer.parseInt(str.split(" ")[0]);
+            xy[i][1] = Integer.parseInt(str.split(" ")[1]);
+        }
+
+        Arrays.sort(xy, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }else{
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+        for (int x[] :xy){
+            for (int y:x){
+                bw.write(y+" ");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
